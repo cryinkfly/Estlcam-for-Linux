5394b751584b424742c613f95c5be8dcdcb0d0e8
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-03-13 15:56:24 +0100

    Create locale-de.sh

---
+++ files/builds/stable-branch/locale/de-DE/locale-de.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Estclam - Setup Wizard (Linux)                                                     #
+# Description:  This is the German translation for the Setup Wizard.                               #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2023-2023                                                                          #
+# Time/Date:    16:00/13.03.2023                                                                   #
+# Version:      0.1                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS FOR ESTCLAM ARE ARRANGED HERE:                                                                                                           #
+###############################################################################################################################################################
+
+SP_LICENSE_ERROR="Achtung! - Die Lizenzvereinbachung wurde nicht akzeptiert! \n Bitte lesen und aktzeptieren Sie die Lizenzvereinbachung, um mit der Installation fortfahren (JA) zu können."
+
+SP_SELECT_SOLIDWORKS_VERSION_SUBTITLE="Estclam - Versionsauswahl"
+SP_SELECT_SOLIDWORKS_VERSION_TEXT="Bitte wählen Sie Ihre Estclam-Version aus, um fortfahren zu können:"
+
+SP_SELECT_SOLIDWORKS_SUBTITLE="Estclam - Installationsdatei auswählen"
+SP_SELECT_SOLIDWORKS_TEXT="Bitte navigieren Sie in den Ordner, wo sich die Estclam-Installationsdatei (EXE) befindet und wählen Sie die Datei aus."
+
+SP_SELECT_OS_VERSION_SUBTITLE="Linux-Distribution - Konfiguration"
+SP_SELECT_OS_VERSION_TEXT="Bitte wählen Sie Ihre Linux-Distribution aus, um die erforderlichen Pakete für die Installation zu installieren:"
+
+SP_EXIT_SUBTITLE="Installation - Abgeschlossen"
+SP_EXIT_TEXT="Die Installation von Estclam ist abgeschlossen und Sie können es jetzt für Ihre Projekte verwenden."
